9771a7ea69ac5e51d87ab408239ed3065c35be6f
Itati02 <89601723+Itati02@users.noreply.github.com>
2021-12-09 04:06:33 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace loteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pdcarta = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pdcarta)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(956, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Barajear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(721, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Sacar Carta";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(901, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 153);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Abiertas";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 23);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(173, 130);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pdcarta);
+            this.groupBox2.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(648, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(179, 240);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cartas";
+            // 
+            // pdcarta
+            // 
+            this.pdcarta.Location = new System.Drawing.Point(0, 19);
+            this.pdcarta.Name = "pdcarta";
+            this.pdcarta.Size = new System.Drawing.Size(179, 221);
+            this.pdcarta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pdcarta.TabIndex = 13;
+            this.pdcarta.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(833, 60);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(62, 55);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Restan";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 36);
+            this.label1.TabIndex = 13;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(721, 366);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 47);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "¡BUENAS!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+           
+           
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1164, 481);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pdcarta)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pdcarta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace loteria
+{
+    public partial class Form1 : Form
+    {
+        //
+        Stack<int> cartas = new Stack<int>();
+        Random rndm = new Random();
+        int j = 0;
+
+        bool vertical = false;
+        bool diagonal = false;
+        bool Horizontal = false;
+        bool PollaG = false;
+        bool DosDiagonales = false;
+
+        //
+
+        private PictureBox[] tabla;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            tabla = new PictureBox[25];
+            inicializarTabla();
+        }
+        private void inicializarTabla()
+        {
+            int r = 0, c = 0;
+
+            int[] cartas = new int[54];
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = i + 1;
+            }
+            Random rnd = new Random();
+            int a, aux;
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = rnd.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+            }
+
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = new PictureBox();
+                tabla[i].Location = new System.Drawing.Point(100 + (c * 90), 25 + (r * 125));
+                tabla[i].Name = "picTabla" + i;
+                tabla[i].Size = new System.Drawing.Size(85, 120);
+                tabla[i].TabIndex = 0 + i;
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].TabStop = false;
+                tabla[i].Image = Image.FromFile(@"cartas\" + (cartas[i]) + ".jpg");
+                //
+                tabla[i].AllowDrop = true;
+                tabla[i].Click += new EventHandler(ponerFicha);
+                
+                //
+                this.Controls.Add(tabla[i]);
+                c++;
+                if (c == 5)
+                {
+                    r++;
+                    c = 0;
+                }
+            }
+           
+        }
+
+        //
+
+
+
+        private void ponerFicha(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).Image = Image.FromFile(@"C:\Users\angyl\source\repos\loteria\loteria\bin\Debug\Fichas.png");
+            ((PictureBox)sender).SizeMode = PictureBoxSizeMode.StretchImage;
+            ((PictureBox)sender).BackColor = Color.Transparent;
+            ((PictureBox)sender).BackgroundImageLayout = ImageLayout.Stretch;
+        }
+        
+        //
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+                //
+                this.listView1.View = View.LargeIcon;
+                this.imageList1.ImageSize = new Size(106, 127);
+                this.listView1.LargeImageList = this.imageList1;
+
+            }      
+
+            private void button1_Click_1(object sender, EventArgs e)
+            {
+
+                inicializarTabla();
+                label1.Text = (54 - cartas.Count()).ToString();
+                bool Visible = false;
+
+                while (!Visible)
+                {
+                    int num = rndm.Next(1, 54);
+                    if (!cartas.Contains(num))
+                    {
+                        pdcarta.Image = Image.FromFile(@"cartas\" + num + ".jpg");
+                        pdcarta.SizeMode = PictureBoxSizeMode.StretchImage;
+                        cartas.Push(num);
+                        this.imageList1.Images.Add(Image.FromFile(@"cartas\" + num + ".jpg"));
+                        ListViewItem item = new ListViewItem();
+                        item.ImageIndex = j;
+                        this.listView1.Items.Add(item);
+                        Visible = true;
+                        j++;
+                    }
+                }
+            }
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            cartas.Clear();
+            imageList1.Images.Clear();
+            listView1.Items.Clear();
+            pdcarta.Image = null;
+            j = 0;
+            label1.Text = "";
+        } //
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\angyl\source\repos\loteria\loteria\bin\Debug\Buenas\buenas1.wav");
+            player.SoundLocation = (@"C:\Users\angyl\source\repos\loteria\loteria\bin\Debug\Buenas\buenas1.wav");
+            player.Play();
+        }
+
+       
+    }
+}
